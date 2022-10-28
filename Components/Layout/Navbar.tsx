194412d07057a26f649545f02767003c26bdd6c8
deanrtaylor1 <deanrtaylor@hotmail.com>
2022-10-28 09:32:06 +0700

    updated mobile navigation functionality, to usestate for changing visibility

--- Components/Layout/Navbar.tsx
+++ Components/Layout/Navbar.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 import { IconContext } from "react-icons";
 import Link from "next/link";
 
 const Navbar: React.FC = () => {
+  const [navBarStyle, setNavBarSyle] = useState<string>(
+    "navbarDropdownList scale-0"
+  );
+  const [hamburgerStyle, setHamburgerStyle] = useState("hamburger");
+
+  const navBarHandler = () => {
+    setNavBarSyle(
+      navBarStyle === "navbarDropdownList scale-0"
+        ? "navbarDropdownList scale-100"
+        : "navbarDropdownList scale-0"
+    );
+    hamburgerHandler();
+  };
+
+  const hamburgerHandler = () => {
+    setHamburgerStyle(
+      hamburgerStyle === "hamburger" ? "hamburger rotate-90" : "hamburger"
+    );
+  };
+
   return (
     <div className="flex justify-between items-center p-2 md:w-4/6">
       <Link href="/">
@@ -11,23 +32,38 @@ const Navbar: React.FC = () => {
           NhimSally
         </h1>
       </Link>
+
       {/* Mobile Nav */}
-      <div className="group">
+      <div onClick={navBarHandler} className="group ">
         <IconContext.Provider
-          value={{ color: "rgb(24 24 27)", className: "global-class-name" }}
+          value={{
+            color: "rgb(24 24 27)",
+            className: "global-class-name 0",
+          }}
         >
-          <GiHamburgerMenu className="hamburger" />
+          <GiHamburgerMenu className={hamburgerStyle} />
         </IconContext.Provider>
-        <div className="navbarDropdownList">
+        <div className={navBarStyle}>
           <ul className="flex flex-col gap-4 pt-12">
+            <Link href="/">
+              <li onClick={navBarHandler} className="navbarDropdownListItem ">
+                Home
+              </li>
+            </Link>
             <Link href="/gallery">
-              <li className="navbarDropdownListItem">Galleries</li>
+              <li onClick={navBarHandler} className="navbarDropdownListItem">
+                Galleries
+              </li>
             </Link>
             <Link href="/booking">
-              <li className="navbarDropdownListItem">Make A Booking</li>
+              <li onClick={navBarHandler} className="navbarDropdownListItem">
+                Make A Booking
+              </li>
             </Link>
             <Link href="/contact">
-              <li className="navbarDropdownListItem">Contact</li>
+              <li onClick={navBarHandler} className="navbarDropdownListItem">
+                Contact
+              </li>
             </Link>
           </ul>
         </div>
@@ -35,6 +71,9 @@ const Navbar: React.FC = () => {
       {/* Tablet and desktop Nav */}
       <div className="hidden md:flex">
         <ul className="flex gap-4 ">
+          <Link href="/">
+            <li className="navbarDropdownListItem">Home</li>
+          </Link>
           <Link href="/gallery">
             <li className="navbarStandardListItem">Galleries</li>
           </Link>
