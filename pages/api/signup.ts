82adac6ada6bc34e41c7cc9fda44245ea0507bcd
deanrtaylor1 <deanrtaylor@hotmail.com>
2022-10-31 11:11:56 +0700

    updated opengraph image

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//comment
 import type { NextApiRequest, NextApiResponse } from "next";
 import User from "../../models/User";
 import connectMongo from "../../mongoose/connectMongo";
