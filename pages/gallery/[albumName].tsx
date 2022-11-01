c3b8b1ffc9a9e82c7af4111b6636c3bc95cf3e74
deanrtaylor1 <deanrtaylor@hotmail.com>
2022-11-01 14:16:21 +0700

    updated useeffect

--- pages/gallery/[albumName].tsx
+++ pages/gallery/[albumName].tsx
@@ -24,6 +24,7 @@ const IndividualGallery: React.FC<GalleryProps> = () => {
         setImageLoaded(true);
       })
       .catch((err) => console.log(err));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
